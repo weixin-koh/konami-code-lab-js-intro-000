3e03fb81c14fc200f5ae6aac622a0ae317877b34
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-03 12:45:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+var index = 0;
+
 function init() {
-  // your code here
+  // Attaching an keydown event listener to document.body
+  document.body.addEventListener("keydown", onKeyDownHandler);
+}
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
