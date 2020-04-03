d5f8c67dc7931a2a5906a5fb019137800d5a4771
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-03 12:44:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+var index = 0;
+
 function init() {
-  // your code here
+  // Attaching an keydown event listener to document.body
+  document.body.addEventListener("keydown", (event) => {
+    // Now, how can we check for which specific key was pressed?
+    event.key
+  }
+}
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
